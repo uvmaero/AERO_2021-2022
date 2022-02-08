4768a5b9206789c4837af3893ce316e9c406ede4
Dom Gasperini <dominicgasperini@gmail.com>
2022-02-08 08:45:14 -0500

    added CAN stuff!

--- DCB/driver-control/Core/Src/main.c
+++ DCB/driver-control/Core/Src/main.c
@@ -33,6 +33,12 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
+/**
+ * @brief TODO
+ * - change all the incorrect CAN params
+ * - 
+ */
+
 // inputs
 #define PIN_START_BUTTON;             28    // ready to drive button
 #define PIN_DRIVE_DIRECTION           26		// drive direction toggle
@@ -75,7 +81,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-CAN_HandleTypeDef hcan1;
+CAN_HandleTypeDef hcan;
 
 /* USER CODE BEGIN PV */
 
@@ -143,6 +149,35 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 
+  // CAN
+  CAN_RxHeaderTypeDef rxHeader; //CAN Bus Transmit Header
+  CAN_TxHeaderTypeDef txHeader; //CAN Bus Receive Header
+  uint8_t canRX[8] = {0,0,0,0,0,0,0,0};  //CAN Bus Receive Buffer
+  CAN_FilterTypeDef canFilter; //CAN Bus Filter
+  uint32_t canMailbox; //CAN Bus Mail box variable
+
+  canFilter.FilterBank = 0;
+  canFilter.FilterMode = CAN_FILTERMODE_IDMASK;
+  canFilter.FilterFIFOAssignment = CAN_RX_FIFO0;
+  canFilter.FilterIdHigh = 0;
+  canFilter.FilterIdLow = 0;
+  canFilter.FilterMaskIdHigh = 0;
+  canFilter.FilterMaskIdLow = 0;
+  canFilter.FilterScale = CAN_FILTERSCALE_32BIT;
+  canFilter.FilterActivation = ENABLE;
+  canFilter.SlaveStartFilterBank = 14;
+
+  txHeader.DLC = 8; // Number of bites to be transmitted max- 8
+  txHeader.IDE = CAN_ID_STD;
+  txHeader.RTR = CAN_RTR_DATA;
+  txHeader.StdId = 0x030;
+  txHeader.ExtId = 0x02;
+  txHeader.TransmitGlobalTime = DISABLE;
+
+  HAL_CAN_ConfigFilter(&hcan,&canFilter); //Initialize CAN Filter
+  HAL_CAN_Start(&hcan); //Initialize CAN Bus
+  HAL_CAN_ActivateNotification(&hcan,CAN_IT_RX_FIFO0_MSG_PENDING);// Initialize CAN Bus Rx Interrupt
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -184,7 +219,8 @@ int main(void)
     // wheelSpeedBL
 
     // send CAN messages
-
+    uint8_t csend[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08}; // Tx Buffer
+    HAL_CAN_AddTxMessage(&hcan,&txHeader,csend,&canMailbox); // Send Message
 
     // *** LCD *** //
     // look for LCD button press 
@@ -277,19 +313,19 @@ static void MX_CAN1_Init(void)
   /* USER CODE BEGIN CAN1_Init 1 */
 
   /* USER CODE END CAN1_Init 1 */
-  hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 5;
-  hcan1.Init.Mode = CAN_MODE_NORMAL;
-  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_14TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_5TQ;
-  hcan1.Init.TimeTriggeredMode = DISABLE;
-  hcan1.Init.AutoBusOff = DISABLE;
-  hcan1.Init.AutoWakeUp = DISABLE;
-  hcan1.Init.AutoRetransmission = DISABLE;
-  hcan1.Init.ReceiveFifoLocked = DISABLE;
-  hcan1.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  hcan.Instance = CAN1;
+  hcan.Init.Prescaler = 5;
+  hcan.Init.Mode = CAN_MODE_NORMAL;
+  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_14TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_5TQ;
+  hcan.Init.TimeTriggeredMode = DISABLE;
+  hcan.Init.AutoBusOff = DISABLE;
+  hcan.Init.AutoWakeUp = DISABLE;
+  hcan.Init.AutoRetransmission = DISABLE;
+  hcan.Init.ReceiveFifoLocked = DISABLE;
+  hcan.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan) != HAL_OK)
   {
     Error_Handler();
   }
@@ -315,9 +351,20 @@ static void MX_GPIO_Init(void)
 
 // *** functions *** //
 
-// welcome & boot screen
 /**
- * @brief 
+ * @brief CAN read message function
+ * 
+ * @param hcan
+ */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rxHeader, canRX); //Receive CAN bus message to canRX buffer
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_3);// toggle PA3 LED
+}
+
+
+/**
+ * @brief welcome & boot screen
  * 
  */
 void welcomeScreen()
@@ -331,9 +378,8 @@ void welcomeScreen()
 }
 
 
-// racing hud: mph(est), battery%, drive direction, coast regen, brake regen
 /**
- * @brief 
+ * @brief racing hud: mph(est), battery%, drive direction, coast regen, brake regen
  * 
  */
 void racingHUD()
@@ -383,9 +429,8 @@ void racingHUD()
 }
 
 
-// battery state, bus voltage, rinehart voltage, power mode
 /**
- * @brief 
+ * @brief battery state, bus voltage, rinehart voltage, power mode
  * 
  */
 void electricalSettings()
@@ -424,9 +469,8 @@ void electricalSettings()
 }
 
 
-// ride height, wheel rpm, coast regen, brake regen
 /**
- * @brief 
+ * @brief ride height, wheel rpm, coast regen, brake regen
  * 
  */
 void rideSettings()
