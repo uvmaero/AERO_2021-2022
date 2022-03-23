ac4d4a94a17cadfed5b3c40f1e2dd2dff523c6c9
Dom Gasperini <dominicgasperini@gmail.com>
2022-03-23 17:46:48 -0400

    testing can

--- RCB/rear-control/Core/Src/main.c
+++ RCB/rear-control/Core/Src/main.c
@@ -57,6 +57,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+
 CAN_HandleTypeDef hcan1;
 
 /* USER CODE BEGIN PV */
@@ -113,6 +114,14 @@ void ADC_Select_CH_WTOUT();         // water temperature out
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+	int _write(int32_t file, uint8_t *ptr, int32_t len) {
+		/* Implement your write code here, this is used by puts and printf for example */
+		int i = 0;
+		for(i=0; i < len; i++) {
+			ITM_SendChar((*ptr++));
+		}
+		return len;
+	}
 
   /* USER CODE END 1 */
 
@@ -134,7 +143,6 @@ int main(void)
   MX_GPIO_Init();
   MX_CAN1_Init();
   MX_ADC1_Init();
-
   /* USER CODE BEGIN 2 */
     // init the CAN filter
 	canFilter.FilterBank = 0;
@@ -182,26 +190,30 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    // poll sensor data
-    pollSensorData();
+	// poll sensor data
+	pollSensorData();
 
-    // read CAN messages
-    if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
-	    Error_Handler();
+	// read CAN messages
+	if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+		Error_Handler();
 
-    // send can messages
-    // BASE
-		uint8_t csend0[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
-		HAL_CAN_AddTxMessage(&hcan1, &txHeader0, csend0, &canMailbox); // Send Message
+	// send can messages
+	// BASE
+	uint8_t csend0[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+	HAL_CAN_AddTxMessage(&hcan1, &txHeader0, csend0, &canMailbox); // Send Message
 
-    // DAQ DATA
-		uint8_t csend1[] = {wheelSpeedBL, wheelSpeedBR, rideHeightBL, rideHeightBR, waterTempIn, waterTempOut, 0x06, 0x07};
-		HAL_CAN_AddTxMessage(&hcan1, &txHeader1, csend1, &canMailbox); // Send Message
+	// DAQ DATA
+	uint8_t csend1[] = {wheelSpeedBL, wheelSpeedBR, rideHeightBL, rideHeightBR, waterTempIn, waterTempOut, 0x06, 0x07};
+	HAL_CAN_AddTxMessage(&hcan1, &txHeader1, csend1, &canMailbox); // Send Message
 
-    // CONTROL SETTINGS
-		uint8_t csend2[] = {brakeLightState, pumpState, fanState, 0x03, 0x04, 0x05, 0x06, 0x07};
-		HAL_CAN_AddTxMessage(&hcan1, &txHeader2, csend2, &canMailbox); // Send Message
+	// CONTROL SETTINGS
+	uint8_t csend2[] = {brakeLightState, pumpState, fanState, 0x03, 0x04, 0x05, 0x06, 0x07};
+	HAL_CAN_AddTxMessage(&hcan1, &txHeader2, csend2, &canMailbox); // Send Message
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
   }
+  /* USER CODE END 3 */
 }
 
 /**
@@ -213,7 +225,8 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  // Configure the main internal regulator output voltage
+  /** Configure the main internal regulator output voltage
+  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
   /** Initializes the RCC Oscillators according to the specified parameters
@@ -230,9 +243,11 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLQ = 2;
   RCC_OscInitStruct.PLL.PLLR = 2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
     Error_Handler();
-
-  // Initializes the CPU, AHB and APB buses clocks
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -241,7 +256,9 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
     Error_Handler();
+  }
 }
 
 /**
@@ -251,14 +268,16 @@ void SystemClock_Config(void)
   */
 static void MX_ADC1_Init(void)
 {
+
   /* USER CODE BEGIN ADC1_Init 0 */
   /* USER CODE END ADC1_Init 0 */
 
-  // ADC_ChannelConfTypeDef sConfig = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
 
   /* USER CODE BEGIN ADC1_Init 1 */
   /* USER CODE END ADC1_Init 1 */
-  // Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
@@ -272,8 +291,11 @@ static void MX_ADC1_Init(void)
   hadc1.Init.DMAContinuousRequests = DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
     Error_Handler();
-  /* Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  }
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = 1;
   sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
@@ -281,7 +303,6 @@ static void MX_ADC1_Init(void)
   {
     Error_Handler();
   }
-  */
   /* USER CODE BEGIN ADC1_Init 2 */
   /* USER CODE END ADC1_Init 2 */
 
@@ -294,6 +315,7 @@ static void MX_ADC1_Init(void)
   */
 static void MX_CAN1_Init(void)
 {
+
   /* USER CODE BEGIN CAN1_Init 0 */
   /* USER CODE END CAN1_Init 0 */
 
@@ -312,10 +334,12 @@ static void MX_CAN1_Init(void)
   hcan1.Init.ReceiveFifoLocked = DISABLE;
   hcan1.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
     Error_Handler();
-    
+  }
   /* USER CODE BEGIN CAN1_Init 2 */
   /* USER CODE END CAN1_Init 2 */
+
 }
 
 /**
